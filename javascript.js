f7dea9ecd9423313a8889202689f017e5e05b84a
Maëlle BANCTEL <maelle.banctel@hotmail.fr>
2020-04-03 11:51:15 +0200

    test menu

--- javascript.js
+++ javascript.js
@@ -1,4 +1,10 @@
-function test(){
-    return alert("mettre le menu ici")
-}
-
+function affichageMenu() {
+    const menu = document.createElement("nav")
+    //A mettre en forme
+    menu.innerHTML = `<div class="menu"><p>MENU</p></div>`
+  
+    var body = document.getElementById("container").parentNode
+    var container = document.getElementById("container")
+    body.insertBefore(menu, container)
+    console.log(body)
+  }
